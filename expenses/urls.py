02d5d4cefc07d86b8a727c844d9de6bce387c39e
Sean Harnett <srharnett@gmail.com>
2011-08-13 23:24:52 -0400

    improved household creation

--- expenses/urls.py
+++ expenses/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls.defaults import patterns, url
 from django.contrib import admin
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.decorators import login_required
 from expenses.models import Person, Household, Transaction
 from expenses.utils import user_in_household, user_is_person
-from expenses.views import HouseholdTransactionsView, HouseholdTransactionCreateView, HouseholdTransactionUpdateView
+from expenses.views import HouseholdTransactionsView, HouseholdTransactionCreateView, HouseholdTransactionUpdateView, HouseholdCreateView
 
 
 admin.autodiscover()
@@ -13,7 +14,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
    url(r'^person/(?P<pk>\d+)$',user_is_person(DetailView.as_view(model=Person, context_object_name='person')), name='person'),
    url(r'^household/(?P<pk>\d+)$',user_in_household(DetailView.as_view(model=Household, context_object_name='household')), name='household'),
-   url(r'^household/create/$',CreateView.as_view(model=Household,success_url="/" ), name='household_create'),
+   url(r'^household/create/$', login_required(HouseholdCreateView.as_view()), name='household_create'),
    url(r'^transaction/create/$',CreateView.as_view(model=Transaction, success_url="/"),name='transaction_create'),
    url(r'^transaction/(?P<pk>\d+)/create/$',HouseholdTransactionCreateView.as_view(),name='household_transaction_create'),
    url(r'^transaction/(?P<pk>\d+)/edit/$',HouseholdTransactionUpdateView.as_view(),name='transaction_edit'),
