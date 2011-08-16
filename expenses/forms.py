e4dfe4546bd4fd991f0ea4d37725450bcb2e9947
Chris Stoafer <cstoafer@gmail.com>
2011-08-15 22:53:33 -0400

    Added validation methods to the form to invite users to a household.  validation includes: checking that the user exists and checking that the user has not yet been invited and is not part of the household.

--- expenses/forms.py
+++ expenses/forms.py
@@ -26,24 +26,50 @@ class HouseholdTransactionForm(ModelForm):
 
     
 
-class InviteToHouseholdForm(ModelForm):
-
+class InviteToHouseholdForm(ModelForm):   
     household = forms.ModelChoiceField(widget=forms.HiddenInput(),queryset=Household.objects.all())
-    invited_user = forms.CharField(max_length=100, label='username')
+    invited_user = forms.CharField(max_length=100, label='username', required=True)
     
     class Meta:
         model = Invited
         exclude = ('user',)
-    
+          
+    def clean_invited_user(self):
+        model = super(InviteToHouseholdForm, self).save(commit=False)
+        check_user_exists = User.objects.filter(username__iexact=self.cleaned_data['invited_user'])
+        check_user_in_household = Household.objects.filter(name__exact=self.cleaned_data['household'].name).filter(persons__user__username__iexact=self.cleaned_data['invited_user'])
+        check_user_invited = Invited.objects.filter(household__name__exact=self.cleaned_data['household'].name).filter(user__username__iexact=self.cleaned_data['invited_user'])
+        
+        if not check_user_exists:
+            raise forms.ValidationError("The username does not exist.")
+        if check_user_in_household:
+            msg_in_household = "This user is already part of this household."
+            raise forms.ValidationError([msg_in_household])
+        if check_user_invited:
+            #existing_user = self.cleaned_data['invited_user']
+            msg_invited = "This user has already been invited to this household"#{{ self.cleaned_data['household'] }}." #, self.cleaned_data['invited_user'], self.cleaned_data['household']
+            raise forms.ValidationError([msg_invited])
+
+        
+        return self.cleaned_data['invited_user']
+                
+            
+#    def clean(self):
+#        check_user_exists = User.objects.filter(username__iexact=self.cleaned_data['invited_user'])
+#        if not check_user_exists: #check_user_exists is None:
+#            raise forms.ValidationError("The username does not exist.")
+           
+#        return self.cleaned_data
+            
+        
     def save(self, commit=True):
-# don't really get this, just copied it from stackoverflow
-		m = super(InviteToHouseholdForm, self).save(commit=False)
-		print self.cleaned_data['invited_user']
-		m.user = User.objects.get(username=self.cleaned_data['invited_user'])
-		if commit:
-			m.save()
+        m = super(InviteToHouseholdForm, self).save(commit=False)
+        m.user = User.objects.get(username=self.cleaned_data['invited_user'])
+        
+        if commit:
+            m.save()
 		
-		return m
+        return m
     
     
 
